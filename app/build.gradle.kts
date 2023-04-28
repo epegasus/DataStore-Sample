c5aa953736263f7f1fdf3304d5613f5732fb2db1
Sohaib <sohaibahmed.hypersoft@gmail.com>
2023-04-28 17:44:46 +0500

    DataStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.protobuf") version "0.9.3" apply false
 }
 
 android {
@@ -31,6 +34,12 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
+
+    /*// For Protobuf
+    java.sourceSets["main"].java {
+        srcDir("src/main/proto")
+    }*/
 }
 
 dependencies {
@@ -40,7 +49,6 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-
     // LiveData
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
 
@@ -49,5 +57,39 @@ dependencies {
 
     // Proto - DataStore
     implementation("androidx.datastore:datastore:1.0.0")
+    implementation("com.google.protobuf:protobuf-javalite:3.18.0")
+    implementation("com.google.protobuf:protobuf-kotlin:3.21.2")
 
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+    implementation("io.grpc:grpc-stub:1.52.0")
+    implementation("io.grpc:grpc-protobuf:1.52.0")
+    implementation("io.grpc:grpc-okhttp:1.52.0")
+
+    implementation("com.google.protobuf:protobuf-java-util:3.21.7")
+    implementation("com.google.protobuf:protobuf-kotlin:3.21.2")
+    implementation("io.grpc:grpc-kotlin-stub:1.3.0")
+
+
+    //implementation("com.google.protobuf:protobuf-javalite:3.18.0")
+    //implementation("com.google.protobuf:protobuf-java:3.22.3")
+    //implementation("com.google.protobuf:protobuf-kotlin-lite:3.18.0")
 }
+/*
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:21.7"
+    }
+
+    // Generates the java Protobuf-lite code for the Protobufs in this project. See
+    // https://github.com/google/protobuf-gradle-plugin#customizing-protobuf-compilation
+    // for more information.
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}*/
